94fbc2fa980201e9a11a1d961cf11bedd00984be
David Blankenship <dblanken@iu.edu>
2022-09-13 15:26:40 -0400

    Create markdown version

--- cv.rb
+++ cv.rb
@@ -9,33 +9,37 @@ require_relative './lib/acts_like_rails'
 
 include ActsLikeRails
 
-DEFAULT_FILENAME = 'resume.yml'
-DEFAULT_ROLE = 'software_engineer'
+DEFAULT_FILENAME = 'resume.yml'.freeze
+DEFAULT_ROLE = 'software_engineer'.freeze
 
 options = {
   role: DEFAULT_ROLE,
-  filename: DEFAULT_FILENAME
+  filename: DEFAULT_FILENAME,
+  output_type: 'html'
 }
 OptionParser.new do |opt|
   opt.on('--role ROLE') { |o| options[:role] = o }
   opt.on('--filename FILENAME') { |o| options[:filename] = o }
+  opt.on('--type TYPE') { |o| options[:output_type] = o }
   opt.on('-h') do
-    puts "Usage: ruby cv.rb --role ROLE --filename FILENAME"
-    puts "role: The subdirectory under templates to use"
-    puts "filename: The YAML file location"
+    puts 'Usage: ruby cv.rb --role ROLE --filename FILENAME'
+    puts 'role: The subdirectory under templates to use'
+    puts 'filename: The YAML file location'
     return 0
   end
 end.parse!
 
 @role = options[:role]
 filename = options[:filename]
+@output_type = options[:output_type]
 
 @config = Config.new(filename: filename)
 
 erb = ERB.new(
   File.read(
-    File.join("./templates/#{@role}/index.html.erb")
-  )
+    File.join("./templates/#{@role}/index.#{@output_type}.erb")
+  ),
+  trim_mode: '-'
 )
 
 output = erb.run(binding)
--- lib/acts_like_rails.rb
+++ lib/acts_like_rails.rb
@@ -2,8 +2,9 @@ module ActsLikeRails
   def render(filename)
     erb = ERB.new(
       File.read(
-        File.join("./templates/#{@role}/_#{filename}.html.erb")
-      )
+        File.join("./templates/#{@role}/_#{filename}.#{@output_type}.erb")
+      ),
+      trim_mode: '-'
     )
 
     erb.result
